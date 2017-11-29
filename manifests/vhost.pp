683affa8127197b39f5dab8f06c5aca9d74f1079
root <root@testmaster.xvg>
2017-11-28 22:14:31 -0500

    fix issues

--- manifests/vhost.pp
+++ manifests/vhost.pp
@@ -12,7 +12,7 @@ define nginx::vhost(
 ){
   $vhost_docroot = "${::nginx::docroot}/${name}"
 
-  file { "/etc/nginx/conf.d/${vhost_dir}/${priority}/${name}.conf": 
+  file { "/etc/nginx/conf.d/${vhost_dir}/${priority}-${name}.conf": 
    ensure  => file,
    content => template("${module_name}/vhost/vhost.conf.erb"),
    mode    => $mode,
